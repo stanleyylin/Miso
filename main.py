8068af6c426a6eabc0c6f88487f0d48c7b70787d
lyriao <96245364+lyriao@users.noreply.github.com>
2023-01-21 15:12:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from commands import Commands
+
+intents = discord.Intents.all()
+# client = commands.Bot()
+
+client = commands.Bot(command_prefix = '+', intents= intents)
+
+client.add_cog(Commands(client))
+
+# TODO: learn about cogs 
+
+@client.event 
+async def on_ready():
+    print("The bot is now ready for use.")
+    print("-----------------------------")
+
+@client.command()
+async def hello(ctx):
+    print("received message")
+    await ctx.send("hola")
+
+with open("token.txt", "r", encoding="utf-8") as f:
+    bottoken = f.read()
+
+client.run(bottoken)
+
+
