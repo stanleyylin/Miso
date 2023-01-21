2e163a48cc02e7f13b99bce9ca6e5b2bd188708c
stanleyylin <stanleylin82@gmail.com>
2023-01-21 17:04:46 -0500

    animation done

--- web-app/src/components/Header.js
+++ web-app/src/components/Header.js
@@ -1,5 +1,12 @@
 import React from "react";
 import "./header.css";
+import InfiniteLooper from "./InfiniteLooper.tsx";
+import demo1 from '../res/demo1.png'
+import demo2 from '../res/demo2.png'
+import demo3 from '../res/demo3.png'
+import deco1 from '../res/deco1.gif';
+import deco2 from '../res/deco2.gif';
+import deco3 from '../res/deco3.gif';
 const Header = () => {
   return (
     <header className="intro">
@@ -9,7 +16,26 @@ const Header = () => {
       <div className="bottom-header">
 
       </div>
+      <InfiniteLooper speed="15" direction="right">
+        <div className="contentBlock contentBlock--one">
+          <img src={demo1} className="scroller-photo"/>
+        </div>
+        <div className="contentBlock contentBlock--one">
+          <img src={demo2} className="scroller-photo"/>
+        </div>
+        <div className="contentBlock contentBlock--one">
+          <img src={deco1} className="scroller-photo"/>
+        </div>
+        <div className="contentBlock contentBlock--one">
+          <img src={demo3} className="scroller-photo"/>
+        </div>
+        <div className="contentBlock contentBlock--one">
+          <img src={deco2} className="scroller-photo"/>
+        </div>
+    </InfiniteLooper>
     </header>
+    
+    
   );
 };
 
