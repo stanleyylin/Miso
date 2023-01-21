0a6b66fdf6b2a7b8b25d3b083e60e158db355eb2
kellyquu <116982102+kellyquu@users.noreply.github.com>
2023-01-21 14:13:06 -0500

    disc bot upddate

---
+++ discord-bot/main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+from commands import Commands
+
+intents = discord.Intents.all()
+TOKEN = "MTA2NjI0MDk4MDI0NTgxMTIwMQ.Gmf1Le.RfbZ_ujG6qt5NUbmUvhziuuxFFg7VD_oT9zATg"
+# client = commands.Bot()
+
+client = commands.Bot(command_prefix = '+', intents= intents)
+
+client.add_cog(Commands(client))
+
+# TODO: learn about cogs 
+
+@client.event 
+async def on_ready():
+    print("The bot is now ready for use.")
+    print("-----------------------------")
+
+@client.command()
+async def hello(ctx):
+    print("received message")
+    await ctx.send("hola")
+
+client.run(TOKEN)
