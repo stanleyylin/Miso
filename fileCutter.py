18e8cb58cc13da39d5a91046907147092c3006e2
pranav <pranavsworkemail@gmail.com>
2023-01-21 01:59:05 -0500

    retrieving first half of database

---
+++ fileCutter.py
@@ -0,0 +1,21 @@
+
+outFile = open("output.txt", "w")
+
+#slicing up final_labels.csv
+inFile = open("final_labels.csv", "r")
+inFile.nextLine
+newLine = ""
+for line in inFile:
+    lineItems = line.strip().split(",")
+    count = 0
+    for item in lineItems:
+        if count == 6 or count == 12:
+            newLine += item + ","
+        count += 1
+    outFile.write(newLine)
+    
+
+
+
+
+    
