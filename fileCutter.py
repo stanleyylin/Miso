13f3793b55dac8a3aec1f4a0f47851612514e787
pranav <pranavsworkemail@gmail.com>
2023-01-21 02:52:36 -0500

    finished databuse file cutter

--- fileCutter.py
+++ fileCutter.py
@@ -1,19 +1,39 @@
 
 outFile = open("output.txt", "w")
+outFile.write("message,isMisogynistic","\n")
 
 #slicing up final_labels.csv
 inFile = open("final_labels.csv", "r")
-inFile.nextLine
+inFile.readline()
 newLine = ""
 for line in inFile:
     lineItems = line.strip().split(",")
-    count = 0
-    for item in lineItems:
-        if count == 6 or count == 12:
-            newLine += item + ","
-        count += 1
-    outFile.write(newLine +"\n")
+    if len(lineItems) < 7:
+        continue
+    if lineItems[-6] != "Nonmisogynistic" and lineItems[-6] != "Misogynistic":
+        continue
+    if len(lineItems[6]) > 1 and lineItems[6][0] == "\"":
+        lineItems[6] = lineItems[6][1:]
 
+    if lineItems[-6] == "Nonmisogynistic":
+        newLine += lineItems[6] + "," + "False"
+    else:
+        newLine += lineItems[6] + "," + "True"
+    outFile.write(newLine +"\n")
+    newLine = ""
+inFile.close()
+
+inFile = open("sexism_data.csv", "r")
+inFile.readline()
+newLine = ""
+for line in inFile:
+    lineItems = line.strip().split(",")
+    if len(lineItems) < 5:
+        continue
+    newLine += lineItems[2] + "," + lineItems[-2]
+    outFile.write(newLine + "\n")
+    newLine = ""
+inFile.close()
 
     
 
