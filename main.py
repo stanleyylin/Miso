59f9c77c620b7797a56fe030ffa0764e60e6fcf8
lyriao <96245364+lyriao@users.noreply.github.com>
2023-01-21 23:39:07 -0500

    1

--- main.py
+++ main.py
@@ -20,12 +20,35 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
+    #if message.author.bot:
+    #    return
     curFile = open("messageHistory.txt","a")
     curFile.write(str(message.id) + ",")
     curFile.write(str(message.content) + ",")
     curFile.write(str(message.author.name) + "\n")
+
+    if message.author.bot:
+        curFile.write(str(message.id) + ",")
+        curFile.write(str(message.content) + ",")
+        curFile.write(str(message.author.name) + "\n")
+        return    
+    
+    import requests
+    url = "//api.estuary.tech/content/add"
+
+    payload={}
+    files=[
+    ('data',('file',open('\Users\lecia\OneDrive\Documents\GitHub\Miso\messageHistory.txt','rb'),'application/octet-stream'))
+    ]
+    headers = {
+    'Accept': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    print(response.text)
+
+    if curFile[1] == "True":
+        await message.author.send("pip")
     problemList = misogynyModel.classifyMessage(message.content)
     await message.channel.send(str(problemList[0]) + ", " + str(problemList[1]) + "%")
     await message.channel.send(str(problemList[2]) + ", " + str(problemList[3]) + "%")
